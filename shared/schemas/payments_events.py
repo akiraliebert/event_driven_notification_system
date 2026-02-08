0e2ba106b8b7c91470c9539f58b74eed5c945166
Ivan Liebert <akiraliebert23@gmail.com>
2026-02-08 19:28:06 +0500

    Delete shared directory

--- shared/schemas/payments_events.py
+++
@@ -1,9 +0,0 @@
-from uuid import UUID
-
-from pydantic import BaseModel
-
-
-class PaymentFailedPayload(BaseModel):
-    payment_id: UUID
-    user_id: UUID
-    reason: str
--- shared/schemas/user_events.py
+++
@@ -1,8 +0,0 @@
-from uuid import UUID
-
-from pydantic import BaseModel, EmailStr
-
-
-class UserRegisteredPayload(BaseModel):
-    user_id: UUID
-    email: EmailStr
